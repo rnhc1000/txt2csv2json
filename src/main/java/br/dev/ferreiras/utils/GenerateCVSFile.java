aaa5ea81ec51e9e54e1fd93f73f47edb3f5378f5
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-03-27 19:44:55 -0300

    HttpRequest fetching Anatel URL blocking API and use of Logger to track progress

--- src/main/java/br/dev/ferreiras/utils/GenerateCVSFile.java
+++ src/main/java/br/dev/ferreiras/utils/GenerateCVSFile.java
@@ -1,25 +1,40 @@
 package br.dev.ferreiras.utils;
 
+import br.dev.ferreiras.App;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import static br.dev.ferreiras.utils.ConsumeAnaBlock.getVersionBlockedSites;
 
 public class GenerateCVSFile {
   public static boolean fileExists(String filePath, String fileName) throws IOException {
+    Logger logger = Logger.getLogger(App.class.getName());
+    logger.info("Task initiated....");
     String fileToBeProcessed = filePath.concat(fileName);
-    boolean response = true;
+    boolean response;
     try {
 
       File handler = new File(fileToBeProcessed);
       if (handler.createNewFile()) {
-        System.out.println("Arquivo de saida criado com sucesso");
-        response = false;
+
+        logger.info("Arquivo de saida criado com sucesso");
+        response = true;
+
       } else {
-        System.out.println("Arquivo ja existe!");
+
+        response = true;
+        logger.info("Arquivo ja existe!");
+
       }
 
     } catch (IOException ex) {
@@ -31,25 +46,70 @@ public class GenerateCVSFile {
     return response;
   }
 
+  public static boolean writeBlockedSites(List<String> blockedSites, String filePath, String fileName) throws IOException {
+    Logger logger = Logger.getLogger(App.class.getName());
+    logger.info("Task had just been submitted!");
+    logger.info("List passed as argument of size " + String.valueOf(blockedSites.size()));
+    boolean isCreated = false;
+    long count = 0L;
+    String path = filePath.concat(fileName);
+
+    try {
+      if (fileExists(filePath, fileName)) {
+
+        try(FileWriter fileWriter = new FileWriter(path)) {
+
+          for (String sites : blockedSites) {
+
+            fileWriter.write(sites);
+            fileWriter.write("\n");
+          }
+        }
+        catch (IOException ex) {
+          throw new RuntimeException();
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    Path fileCreated = Paths.get(path);
+    count = Files.size(fileCreated);
+    int numberOfLines;
+    try (Stream<String> fileStream = Files.lines(fileCreated)) {
+      numberOfLines = (int) fileStream.count();
+    }
+
+    logger.info("File created at " + path + " " + String.valueOf(numberOfLines) + " lines");
+    logger.info("Task had just finished! File created with success!");
+
+    isCreated = true;
+    return isCreated;
+  }
   public static Set<String> readBlockedSites(String filePath, String fileName) {
     String line = null;
     String path = filePath.concat(fileName);
 
     List<String> blockedSites = new ArrayList<>();
     try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
       while ((line = br.readLine()) != null) {
         String[] lines = line.split("\\s");
         blockedSites.add(lines[0]);
       }
+
     } catch (IOException e) {
+
       throw new RuntimeException(e);
+
     }
 
     return new TreeSet<>(blockedSites);
   }
 
-  public static void generateCSV(Set<String> blockedSites) throws IOException {
 
+
+  public static void generateCSV(Set<String> blockedSites, String filePath, String fileName) throws IOException {
+  Logger logger = Logger.getLogger(App.class.getName());
 //    enum blockedHeaders {
 //      id,
 //      serial,
@@ -57,11 +117,11 @@ public class GenerateCVSFile {
 //      url,
 //      status
 //    }
-    String filePath = "/home/rferreira/Downloads/";
-    String fileName = "ascendingBlockedSitesMar24.txt";
+//    String filePath = "/home/rferreira/Downloads/";
+//    String fileName = "ascendingBlockedSitesMar24.txt";
     boolean OK = fileExists(filePath, fileName);
-    String serial = "2024031501";
-    System.out.println(OK);
+    logger.info("Fetching serial of most recent version available of sites");
+    String serial = getVersionBlockedSites().trim();
     int count = 1;
     //String formatter = DateFormatter()
     if (OK) {
@@ -76,9 +136,8 @@ public class GenerateCVSFile {
           sb = new StringBuilder(sites.concat("," + serial + "," + LocalDateTime.now() + "," + "ativo"));
           sb.insert(0, count + ",");
           sortedList.add(sb);
-          count++;
         }
-        System.out.println(blockedSites.size());
+       logger.info("Number of blocked sites " + String.valueOf(blockedSites.size()));
         for (StringBuilder sites : sortedList) {
           fw.write(sites.toString());
           fw.write("\n");
@@ -89,7 +148,7 @@ public class GenerateCVSFile {
         throw new IOException("File not accessible! " + ex.getMessage());
       }
     }
-    System.out.println("Arquivo Criado" + count + " linhas!");
+    logger.info("Arquivo Criado" + String.valueOf(count) + " linhas!");
   }
 
   public static void generateJSON(String filePath, String fileName) throws IOException {
@@ -113,20 +172,7 @@ public class GenerateCVSFile {
 //      }
     try (BufferedReader br = new BufferedReader(new FileReader(path))) {
       while ((line = br.readLine()) != null) {
-        String[] csvRecord = line.split(delimiter);
-//        System.out.println(
-//                "id: " + csvRecord[0] +
-//                        " url: " + csvRecord[1] +
-//                        " serial: " + csvRecord[2] +
-//                        " created_at: " + csvRecord[3] +
-//                        " status: " + csvRecord[4]
-//        );
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id", csvRecord[0]);
-        jsonObject.put("url", csvRecord[1]);
-        jsonObject.put("serial", csvRecord[2]);
-        jsonObject.put("created_at", csvRecord[3]);
-        jsonObject.put("status", csvRecord[4]);
+        JSONObject jsonObject = getJsonObject(line, delimiter);
 
         arrayOfJsonObjects.put(jsonObject);
       }
@@ -135,6 +181,24 @@ public class GenerateCVSFile {
     }
 
     recordJsonFile(arrayOfJsonObjects,filePath);
+  }
+
+  private static JSONObject getJsonObject(String line, String delimiter) {
+    String[] csvRecord = line.split(delimiter);
+//        System.out.println(
+//                "id: " + csvRecord[0] +
+//                        " url: " + csvRecord[1] +
+//                        " serial: " + csvRecord[2] +
+//                        " created_at: " + csvRecord[3] +
+//                        " status: " + csvRecord[4]
+//        );
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("id", csvRecord[0]);
+    jsonObject.put("url", csvRecord[1]);
+    jsonObject.put("serial", csvRecord[2]);
+    jsonObject.put("created_at", csvRecord[3]);
+    jsonObject.put("status", csvRecord[4]);
+    return jsonObject;
   }
 
   public static void recordJsonFile(JSONArray json, String filePath) throws IOException {
